569cca02e380fc7e8e18fcf88fac3ed2fb7bb0f4
Marcin Gol <golmarcin@gmail.com>
2018-05-16 21:45:48 +0200

    update Readme to be more clarified - remove random README :fire:

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Networker-Server
 Networker app serverless architecture back-end
+
+
+App based on Serverless and AWS FaaS - experimenting with new technologies on back - end ;)
---
+++ index.js
@@ -0,0 +1,7 @@
+const serverless = require('serverless-http');
+const express = require('express');
+const app = express();
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+module.exports.handler = serverless(app);
